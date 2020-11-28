13233d334c40d83e1b917aa0ee3185a9a12843a9
Marco Baiad <marcobaiad@gmail.com>
2020-11-28 08:55:35 -0300

    Updates

---
+++ precache-manifest.646705a5862ed72b003a695077f9c38c.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5dbca9cbd9c2e6fae22a0eb68274a46",
+    "url": "/norte-tabaco/index.html"
+  },
+  {
+    "revision": "85ab91b7129ec62f6440",
+    "url": "/norte-tabaco/static/css/main.d2c36580.chunk.css"
+  },
+  {
+    "revision": "bddc303134715054179a",
+    "url": "/norte-tabaco/static/js/2.b339632a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/norte-tabaco/static/js/2.b339632a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85ab91b7129ec62f6440",
+    "url": "/norte-tabaco/static/js/main.4bf68f8b.chunk.js"
+  },
+  {
+    "revision": "cb531d876a136325b300",
+    "url": "/norte-tabaco/static/js/runtime-main.7cb7f9d4.js"
+  },
+  {
+    "revision": "ddace4671397b1448d40badd9ae6d5be",
+    "url": "/norte-tabaco/static/media/avionNocturno-F6600.ddace467.png"
+  },
+  {
+    "revision": "e89f25f4c06570196506b20cd5baa909",
+    "url": "/norte-tabaco/static/media/bengalita-F2201.e89f25f4.jpg"
+  },
+  {
+    "revision": "af68ed651a57105d32259ef80769b8aa",
+    "url": "/norte-tabaco/static/media/bombaColorTornado-F8817.af68ed65.png"
+  },
+  {
+    "revision": "1cbf19e5f619c82a4dc40ae83482d1c0",
+    "url": "/norte-tabaco/static/media/botellitaChampagneF9970.1cbf19e5.png"
+  },
+  {
+    "revision": "bcb5c330247e03a91a360a3d8965f044",
+    "url": "/norte-tabaco/static/media/boxer@3x.bcb5c330.png"
+  },
+  {
+    "revision": "0048e0e289cf26c5c76a6a14c417dbb8",
+    "url": "/norte-tabaco/static/media/candelaChica-F2254.0048e0e2.jpg"
+  },
+  {
+    "revision": "31383b941d913390bc5dc857ed436dc9",
+    "url": "/norte-tabaco/static/media/cañaBomba-F3357.31383b94.png"
+  },
+  {
+    "revision": "fed91582cc2c36a9ff0b4504bf7f2299",
+    "url": "/norte-tabaco/static/media/colitaDragon-F9960.fed91582.jpg"
+  },
+  {
+    "revision": "3817b06c0d496b504acc12ec568c8537",
+    "url": "/norte-tabaco/static/media/estrellita-F2257-150.3817b06c.jpg"
+  },
+  {
+    "revision": "b0b6376e31a918eb88439d46ad55fdc6",
+    "url": "/norte-tabaco/static/media/estrellita-f2257jpg.b0b6376e.jpg"
+  },
+  {
+    "revision": "88732640c1423ff7683b405e66a9d635",
+    "url": "/norte-tabaco/static/media/fuegoLandia.88732640.png"
+  },
+  {
+    "revision": "f397cb30eb6469834257bbd6ad4c2404",
+    "url": "/norte-tabaco/static/media/giratorio6chisperos-F6660.f397cb30.png"
+  },
+  {
+    "revision": "e4d7f6f08262e87753a0f08f65a6ac6a",
+    "url": "/norte-tabaco/static/media/giratorioAbejitasTornado-F9910.e4d7f6f0.png"
+  },
+  {
+    "revision": "218d74ff4ae9f0a67069378bcc534193",
+    "url": "/norte-tabaco/static/media/huevoDragon-f9920.218d74ff.png"
+  },
+  {
+    "revision": "b986767b1bd0092efab998c5d8135f52",
+    "url": "/norte-tabaco/static/media/liverpool@3x.b986767b.png"
+  },
+  {
+    "revision": "252227e5690fe3dfe8d07ded2bb6e97e",
+    "url": "/norte-tabaco/static/media/logoNT1.252227e5.png"
+  },
+  {
+    "revision": "74210ab75087c921142d09af31920214",
+    "url": "/norte-tabaco/static/media/magicColours-F3336.74210ab7.png"
+  },
+  {
+    "revision": "f6e2c93c604c89acd3209172d5734769",
+    "url": "/norte-tabaco/static/media/magnum-F3347.f6e2c93c.png"
+  },
+  {
+    "revision": "6862e1c66f216ee2deadae5854732034",
+    "url": "/norte-tabaco/static/media/magnumII-f3344.6862e1c6.png"
+  },
+  {
+    "revision": "a91babfa47ed7252427abbd26a72c2c9",
+    "url": "/norte-tabaco/static/media/mariposas-F6610.a91babfa.png"
+  },
+  {
+    "revision": "e11adbcc6e15b3dd6c70b11add2123ed",
+    "url": "/norte-tabaco/static/media/master@3x.e11adbcc.png"
+  },
+  {
+    "revision": "974a8f0be6d8d11e8cd4fbf0426e8e94",
+    "url": "/norte-tabaco/static/media/melbourne.974a8f0b.jpg"
+  },
+  {
+    "revision": "ff32a2bab720ddbf3f11b246b1693397",
+    "url": "/norte-tabaco/static/media/milenio.ff32a2ba.jpg"
+  },
+  {
+    "revision": "e69a53b41dfdfb1df98043f75d8c8af7",
+    "url": "/norte-tabaco/static/media/nuevoBoom-f9935.e69a53b4.jpg"
+  },
+  {
+    "revision": "aa976dda0753bd12061854cc0b75a17c",
+    "url": "/norte-tabaco/static/media/ovniDancingNew-F9990.aa976dda.png"
+  },
+  {
+    "revision": "18f0fa313d8632e00861cd98f02e0d15",
+    "url": "/norte-tabaco/static/media/pier@3x.18f0fa31.png"
+  },
+  {
+    "revision": "130b9dcbea9ed86f80ff345177f5f365",
+    "url": "/norte-tabaco/static/media/redpoint@3x.130b9dcb.png"
+  },
+  {
+    "revision": "0a7ebd8d0506d562ae52dae7f91d4beb",
+    "url": "/norte-tabaco/static/media/remolinoLuces-F6636.0a7ebd8d.png"
+  },
+  {
+    "revision": "c006847e1a7c1c2c6b26b965a6405381",
+    "url": "/norte-tabaco/static/media/superFantasia-F3394.c006847e.png"
+  }
+]);
